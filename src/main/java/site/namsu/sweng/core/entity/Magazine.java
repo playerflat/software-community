0bb1166f183d2b2a71a0b04df837822c3136a5b5
jeongwhanchoi <jeongwhan.choi@icloud.com>
2018-12-01 19:19:26 +0900

    <DB> Magazine Entity 추가

---
+++ src/main/java/site/namsu/sweng/core/entity/Magazine.java
@@ -0,0 +1,17 @@
+package site.namsu.sweng.core.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Magazine {
+    private int magazineNumber;
+    private String title;
+    private String text;
+    private String date;
+}
