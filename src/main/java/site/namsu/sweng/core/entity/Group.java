62a32ad9302898739d95d06cc524020be4d885de
jeongwhanchoi <jeongwhan.choi@icloud.com>
2018-12-01 19:42:43 +0900

    <DB> Group Entity 수정

--- src/main/java/site/namsu/sweng/core/entity/Group.java
+++ src/main/java/site/namsu/sweng/core/entity/Group.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @Author : Hyunwoong
@@ -18,27 +17,29 @@ public class Group implements Serializable {
 
     private @NonNull int groupNumber; // <-- 기본키
     private String groupName;
-    private List<String> groupMember = new ArrayList<>();
+    private ArrayList<String> groupMembers = new ArrayList<>();
+    private ArrayList<Message> messages = new ArrayList<>();
+
 
     public boolean addMember(String stdNumber) {
         if (!searchMember(stdNumber)) {
-            return groupMember.add(stdNumber);
+            return groupMembers.add(stdNumber);
         } else return false;
     }
 
     public boolean deleteMemeber(String stdNumber) {
         if (searchMember(stdNumber)) {
-            return groupMember.remove(stdNumber);
+            return groupMembers.remove(stdNumber);
         } else return false;
     }
 
     public void clearGroup() {
-        if (groupMember == null) groupMember = new ArrayList<>();
-        groupMember.clear();
+        if (groupMembers == null) groupMembers = new ArrayList<>();
+        groupMembers.clear();
     }
 
     public boolean searchMember(String stdNumber) {
-        if (groupMember == null) groupMember = new ArrayList<>();
-        return groupMember.contains(stdNumber);
+        if (groupMembers == null) groupMembers = new ArrayList<>();
+        return groupMembers.contains(stdNumber);
     }
 }
