faa07530fbe905d192b115f23e8067c422cac8c6
gusdnd852 <gusdnd852@naver.com>
2018-12-26 11:10:15 +0900

    clean project

--- src/main/table/MagazineTable.sql
+++
@@ -1,8 +0,0 @@
-create table mingus.magazine (
-  magazineNumber int(10) auto_increment primary key,
-  stdNumber      varchar(9),
-  title          varchar(50),
-  name           varchar(10),
-  contents       text,
-  date           varchar(10)
-);
