d5f503147b162a7607666d178cc1aad08b5f8779
gusdnd852 <gusdnd852@naver.com>
2018-12-26 11:49:30 +0900

    no message

--- src/main/table/BoardTable.sql
+++
@@ -1,8 +0,0 @@
-create table mingus.board (
-  boardNumber int(10) auto_increment primary key,
-  stdNumber   varchar(20),
-  name        varchar(10),
-  title       varchar(50),
-  contents    text,
-  date        varchar(10)
-);
--- src/main/table/MessageTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE mingus.message (
-  messageNumber int(100) AUTO_INCREMENT PRIMARY KEY,
-  groupName     varchar(20),
-  name          varchar(10),
-  contents      text,
-  time          varchar(20)
-);
